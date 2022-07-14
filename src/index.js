6f32d43cd4b5af64a043c2390a243055b9c08c15
Teresa <teriv1707@gmail.com>
2022-07-14 22:48:44 +0200

    React.Fragment simplified: <></>

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 
 function App() {
     return (
-        <React.Fragment>
+        <>
             <h1>Hello App.js</h1>;
             <ul>
                 <li>list1</li>
                 <li>list2</li>
                 <li>list3</li>
             </ul>
-        </React.Fragment>
+        </>
     );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
