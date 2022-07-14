d2c0e17d4c8b2a4dda147e74099ab231ab23a7db
Teresa <teriv1707@gmail.com>
2022-07-14 22:42:19 +0200

    react element

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const h1 = <h1>"hello world"</h1>;
+const el = (
+    <div>
+        <div>
+            <ul>
+                <li>list1</li>
+                <li>list2</li>
+                <li>list3</li>
+            </ul>
+        </div>
+        <p>lorem</p>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(h1);
+root.render(h1, el);
