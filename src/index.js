c5f317b44e7caf8f8313b572cde109dc1366005a
Teresa <teriv1707@gmail.com>
2022-07-14 22:59:11 +0200

    components creation with arrow function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function App() {
+const Header = () => {
+    return <h1>Hello App.js</h1>;
+};
+const AppList = () => {
     return (
-        <>
-            <h1>Hello App.js</h1>;
+        <div>
+            {" "}
             <ul>
                 <li>list1</li>
                 <li>list2</li>
                 <li>list3</li>
             </ul>
+        </div>
+    );
+};
+
+function App() {
+    return (
+        <>
+            <Header />
+            <AppList />
         </>
     );
 }
