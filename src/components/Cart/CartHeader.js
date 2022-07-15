17045253d8693db5bdd3258e62f15ad48aa41525
Teresa <teriv1707@gmail.com>
2022-07-15 13:32:37 +0200

    added scss

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import styles from './CartHeader.module.css'
+import './CartHeader.scss'
 
 export const CartHeader = () => {
     return (
         <>
-            <div className={styles.cartHeader}>
+            <div className="cartHeader">
                 <div>0</div>
                 <div>0$</div>
             </div>
