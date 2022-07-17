463b5ed696aeba429706a34ecfffc842cfe4e786
Teresa <teriv1707@gmail.com>
2022-07-17 19:59:20 +0200

    destructurisation

--- src/components/Products/ProductListItem.js
+++ src/components/Products/ProductListItem.js
@@ -2,16 +2,22 @@ import { Button, Card, CardActions, CardContent } from '@mui/material'
 import React from 'react'
 import './ProductListItem.scss'
 
-export const ProductListItem = (props) => {
+export const ProductListItem = ({
+    name,
+    description,
+    type,
+    capacity,
+    price,
+}) => {
     return (
         <>
             <Card>
                 <CardContent>
-                    <h4>{props.name}</h4>
-                    <p>This is {props.name}</p>
-                    <div>Type: {props.type}</div>
-                    <div>Capacity: {props.capacity}</div>
-                    <div>{props.price} $</div>
+                    <h4>{name}</h4>
+                    <p>{description}</p>
+                    <div>Type: {type}</div>
+                    <div>Capacity: {capacity}</div>
+                    <div>{price} $</div>
                 </CardContent>
                 <CardActions className="add-to-cart-block">
                     <Button variant="contained">Add to cart</Button>
