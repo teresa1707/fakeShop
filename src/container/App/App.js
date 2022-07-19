cbc8ca0b0051ecd2287058ccfc917ae979636e40
Teresa <teriv1707@gmail.com>
2022-07-19 23:11:06 +0200

    data in App - level up State

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Footer } from 'container/Footer/Footer'
 import { Header } from 'container/Header/Header'
 import { Main } from 'container/Main/Main'
 import { CssBaseline } from '@mui/material'
 
 export const App = () => {
+    const [cartData, setCartData] = useState({ count: 1, price: 1 })
+
+    const addToCart = () => {}
+
     return (
         <>
             <CssBaseline />
-            <Header />
+            <Header cartData={cartData} />
             <Main />
             <Footer />
         </>
