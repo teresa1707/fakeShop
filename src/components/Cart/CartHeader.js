cc0d33422115565175adcdfebb37f87b2309b8f0
Teresa <teriv1707@gmail.com>
2022-07-21 23:43:26 +0200

    lodash keys

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,11 +1,12 @@
 import { object } from 'prop-types'
 import React from 'react'
 import './CartHeader.scss'
+import { keys } from 'lodash'
 
 export const CartHeader = ({ productsInCart }) => {
     return (
         <div className="cartHeader" key={id}>
-            {Object.keys(productsInCart).map((id) => (
+            {keys(productsInCart).map((id) => (
                 <div>
                     {id}:{productsInCart[id]}
                 </div>
