afe67e4484c8bebfcfef35c1ba73ce378164665e
Teresa <teriv1707@gmail.com>
2022-07-29 19:13:59 +0200

    component QUANTITY

--- src/components/Cart/CartProductListItemExtended.js
+++ src/components/Cart/CartProductListItemExtended.js
@@ -2,6 +2,7 @@ import { Button, Card, CardContent, Grid } from '@mui/material'
 import React from 'react'
 import './CartProductListExtended.scss'
 import DeleteIcon from '@mui/icons-material/Delete'
+import { Quantity } from 'components/Quantity/Quantity'
 
 export const CartProductListItemExtended = ({
     product,
@@ -22,6 +23,8 @@ export const CartProductListItemExtended = ({
                     <div>{product.name}</div>
                     <p>Price for one item : {product.price}</p>
                     <p>Count: {productCount}</p>
+                    <Quantity count={productCount} />
+                    <br />
                     <Button
                         onClick={() => removeProductFromCart(product.id)}
                         variant="outlined"
