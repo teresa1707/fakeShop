0e483069bef8a2f8157119ad99b76bad977e696a
Teresa <teriv1707@gmail.com>
2022-07-30 10:31:06 +0200

    created redux store productsLikeReducer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './container/App/App'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux'
+import { productsLikeReducer } from 'redux/productsLikeReducer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const store = createStore(productsLikeReducer)
 root.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
 )
---
+++ src/redux/productsLikeReducer.js
@@ -0,0 +1,7 @@
+export const productsLikeReducer = (
+    state = {
+        1: true,
+        2: true,
+    },
+    action
+) => state
