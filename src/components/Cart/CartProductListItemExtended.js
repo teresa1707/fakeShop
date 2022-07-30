fa4a679f7ea89765d37a3f08b68ccb480a5caf78
Teresa <teriv1707@gmail.com>
2022-07-30 13:28:53 +0200

    buttons like-dislike in productsExtended

--- src/components/Cart/CartProductListItemExtended.js
+++ src/components/Cart/CartProductListItemExtended.js
@@ -3,14 +3,19 @@ import React from 'react'
 import './CartProductListExtended.scss'
 import DeleteIcon from '@mui/icons-material/Delete'
 import { Quantity } from 'components/Quantity/Quantity'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import { useDispatch, useSelector } from 'react-redux'
 
 export const CartProductListItemExtended = ({
     product,
     productCount,
     removeProductFromCart,
     changeProductQuantity,
-    minCount,
 }) => {
+    const isLiked = useSelector((state) => state[product.id])
+    const dispatch = useDispatch()
+
     return (
         <Grid item xs={12} sm={6}>
             <Card className="cart-product-extended">
@@ -46,6 +51,27 @@ export const CartProductListItemExtended = ({
                         variant="outlined"
                     >
                         <DeleteIcon />
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() =>
+                            isLiked
+                                ? dispatch({
+                                      type: 'DISLIKE',
+                                      id: product.id,
+                                  })
+                                : dispatch({
+                                      type: 'LIKE',
+                                      id: product.id,
+                                  })
+                        }
+                    >
+                        {' '}
+                        {isLiked ? (
+                            <FavoriteIcon />
+                        ) : (
+                            <FavoriteBorderIcon />
+                        )}{' '}
                     </Button>
                 </CardContent>
             </Card>
